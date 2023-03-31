b14a0a09a47b7f5d3eedd3738cf6062a6b153e19
Juan Cardona (@EPAM) <juan_cardona@epam.com>
2023-03-30 19:53:16 -0500

    Adding Lambda Expressions

--- src/main/java/com/epam/rd/coffeestore/Main.java
+++ src/main/java/com/epam/rd/coffeestore/Main.java
@@ -4,17 +4,9 @@ import com.epam.rd.coffeestore.common.Function;
 
 public class Main {
     public static void main(String[] args) {
-        Function<Coffee,Integer> noDiscount = new Function<>() {
-                public Integer apply(Coffee coffee) {
-                    return coffee.prices;
-                }
-            };
+        Function<Coffee,Integer> noDiscount = (coffee) -> coffee.prices;
 
-        Function<Coffee,Integer> discount = new Function<>() {
-                public Integer apply(Coffee coffee) {
-                    return coffee.prices / 3;
-                }
-            };
+        Function<Coffee,Integer> discount = (coffee) -> coffee.prices / 3;
 
         CreditCard cc = new CreditCard();
         Purchase pNoDiscount = CoffeeStore.buyCoffee(cc, noDiscount);
